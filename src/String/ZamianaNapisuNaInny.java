3eba1961fc07a890d6cb8955ddb284561472540b
dubiel.szymon@gmail.com <dubiel.szymon@gmail.com>
2019-06-25 20:51:29 +0200

    odleglosc Levenshteina

--- src/String/ZamianaNapisuNaInny.java
+++ src/String/ZamianaNapisuNaInny.java
@@ -1,33 +1,40 @@
 package String;
 
 public class ZamianaNapisuNaInny {
-
     private static String textX = "ATGATCGGCAT";
     private static String textY = "CAATGTGAATC";
 
+    static int[][] cost = new int[textX.length()+ 1][textY.length() +1];
+    static String[][] op = new String[textX.length()+ 1][textY.length() +1];
 
     public static void main(String[] args) {
 
+        levenshtein();
+        String sub = levenshtienSubstring(op, textX.length() ,textY.length());
 
-
-        System.out.println("Najdłuzszy wspólny podwyraz to: " );
+        System.out.println(sub);
     }
 
     // odległości Levenshteina. Najmniejsza liczba działań prostych, przeprowadzająca jeden napis w drugi.
-    private static int[][] levenshtein(int[][] l) {
-        int cc = -1;
-        int cr = 1;
-        int cd = 2;
-        int ci =2;
+    private static void levenshtein() {
+        int cc = -1; // cost of copy
+        int cr = 1; // cost of replace
+        int cd = 2; // cost of delete
+        int ci =2; // cost of insert
         int i,j;
-        int[][] cost = new int[textX.length()+ 1][textY.length() +1];
-        String[][] op = new String[textX.length()+ 1][textY.length() +1];
 
-        for(i =1;i < textX.length(); i++)
+
+        for(i =1;i < textX.length() +1; i++)
         {
             cost[i][0] = cd * i;
             op[i][0] = "usun "+ String.valueOf(textX.charAt(i-1));
         }
+        for(j=1; j<textY.length()+1;j++)
+        {
+            cost[0][j] = ci * j;
+            op[0][j] = "wstaw " + String.valueOf(textY.charAt(j-1));
+        }
+
 
         for (i = 1; i < textX.length()+1; i++)
         {
@@ -35,32 +42,44 @@ public class ZamianaNapisuNaInny {
             {
                 if (textX.charAt(i-1) == textY.charAt(j-1))
                 {
-                    l[i][j] = l[i-1][j-1] + 1;
+                    cost[i][j] = cost[i-1][j-1] + cc;
+                    op[i][j] =  "kopiuj " + String.valueOf(textX.charAt(i-1));
+                } else {
+                    cost[i][j] = cost[i-1][j-1] + cr;
+                    op[i][j] =  "zastap " + String.valueOf(textX.charAt(i-1)) + " przez " + String.valueOf(textY.charAt(j-1));
+                }
+
+                if(cost[i-1][j] + cd < cost[i][j])
+                {
+                    cost[i][j] = cost[i-1][j] + cd;
+                    op[i][j] =  "usun " + String.valueOf(textX.charAt(i-1));
+                }
+
+                if(cost[i][j-1] + ci < cost[i][j])
+                {
+                    cost[i][j] = cost[i][j-1] + ci;
+                    op[i][j] =  "wstaw " + String.valueOf(textY.charAt(j-1));
                 }
             }
         }
-        return l;
+        // wynikiem jest minimalny koszt operacji - cost[i][j]
     }
 
-    //obliczeanie max indeksu i jego połozenia
-    public static int[] findMaximumIndex(int[ ][ ] a)
+    //wypisanie krok po kroku operacji jakie trzeba wykonac
+    public static String levenshtienSubstring(String[][] op, int i, int j)
     {
-        int maxVal = 0;
-        int[] answerArray = new int[3];
-        for(int row = 0; row < a.length; row++)
+        if (i == 0 && j ==0)
         {
-            for(int col = 0; col < a[row].length; col++)
-            {
-                if(a[row][col] > maxVal)
-                {
-                    maxVal = a[row][col];
-                    answerArray[0] = row;
-                    answerArray[1] = col;
-                    answerArray[2] = maxVal;
-                }
-            }
+            return "";
         }
-        return answerArray;
+        if (op[i][j].startsWith("k") || op[i][j].startsWith("z"))
+        {
+            return levenshtienSubstring(op,i-1,j-1) + ", " + op[i][j];
+        } else if (op[i][j].startsWith("u")) {
+            return levenshtienSubstring(op,i-1,j) + ", " + op[i][j];
+        } else {
+            return levenshtienSubstring(op,i,j-1) + ", " + op[i][j];
+        }
     }
 
 
